9fb05226fcefe6725507bd037aeeadc82ed0ecea
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-24 02:04:12 +0000

    Completed problem 1

---
+++ Exercise4/Exercise4/Kid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Kid
+    {
+        public virtual void PrintMessage()
+        {
+            Console.WriteLine("I am an instance of the Kid class");
+        }
+    }
+}
--- Exercise4/Exercise4/Program.cs
+++ Exercise4/Exercise4/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Kid kid = new Kid();
+            kid.PrintMessage();
         }
     }
 }
